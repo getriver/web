9f2851c2c9100d99a0dc34aaba952ee4cf1cc509
lunaisnotaboy <her@mint.lgbt>
2021-11-17 11:58:56 -0500

    Add Tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,36 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      './app/index.html',
+      './app/components/**/*.hbs',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/.]+/g) || []
+  }
+}
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    sassOptions: {
-      includePaths: ['node_modules/modern-normalize'],
-    },
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss'),
+          ...isProduction ? [purgeCSS] : []
+        ]
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
